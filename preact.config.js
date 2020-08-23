1495e3bbd3a8bdc1d17256630b6091acc9baf2d5
qidian <qidian990107@163.com>
2020-08-23 01:10:11 -0700

    Set up umd output format

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const path = require('path');
 
 export default {
   /**
@@ -12,52 +13,56 @@ export default {
    * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
    **/
   webpack(config, env, helpers, options) {
-      delete config.entry.polyfills;
-      config.output.filename = "[name].js";
+    delete config.entry.polyfills;
+    // config.output.filename = "[name].js";
 
-      config.plugins = config.plugins.filter(plugin => !(plugin instanceof MiniCssExtractPlugin));
-      config.module.rules = config.module.rules.map(rule => {
-        if (rule.loader === 'babel-loader') {
-          const use = [
-            {
-              loader: 'babel-loader',
-              options: rule.options
-            },
-            {
-              loader: 'ts-loader'
-            }
-          ];
-          return {
-            ...rule,
-            loader: undefined,
-            options: undefined,
-            use
-          };
-        }
-        if (rule.use) {
-          return {
-            ...rule,
-            use: rule.use.reduce((acc, loader) => {
-              loader !== MiniCssExtractPlugin.loader ? acc.push(loader) : acc.push(
-                'style-loader');
-    
-              return acc;
-            }, []),
-          };
-        }
-    
-        return rule;
-      });
+    config.output.path = path.resolve(__dirname, 'dist');
+    config.output.filename = 'index.umd.js';
+    config.output.libraryTarget = 'umd';
 
-      // Use any `index` file, not just index.js
-      config.resolve.alias["preact-cli-entrypoint"] = resolve(
-          process.cwd(),
-          "src",
-          "index"
-      );
-
-      if (env.production) {
-        config.output.libraryTarget = 'umd';
+    config.plugins = config.plugins.filter(plugin => !(plugin instanceof MiniCssExtractPlugin));
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === 'babel-loader') {
+        const use = [
+          {
+            loader: 'babel-loader',
+            options: rule.options
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ];
+        return {
+          ...rule,
+          loader: undefined,
+          options: undefined,
+          use
+        };
       }
+      if (rule.use) {
+        return {
+          ...rule,
+          use: rule.use.reduce((acc, loader) => {
+            loader !== MiniCssExtractPlugin.loader ? acc.push(loader) : acc.push(
+              'style-loader');
+
+            return acc;
+          }, []),
+        };
+      }
+
+      return rule;
+    });
+
+    // Use any `index` file, not just index.js
+    config.resolve.alias["preact-cli-entrypoint"] = resolve(
+      process.cwd(),
+      "src",
+      "index"
+    );
+
+    if (env.production) {
+      config.output.libraryTarget = 'umd';
+    }
   }
 };
